40552ad87c848332f480207959f9fd01590b51bf
sergioifg94 <sergioifg94@gmail.com>
2016-02-11 12:22:22 +0100

    Changed 'email' to 'login' in request

--- src/main/java/controller/VoterController.java
+++ src/main/java/controller/VoterController.java
@@ -16,13 +16,13 @@ public class VoterController {
     /**
      * Process a request to /voter. Checks that the given password matches a voter, and, if it does, respond with
      * data about the voter
-     * @param email Email of the voter
+     * @param login Email of the voter
      * @param password Password of the voter
      * @return Voter
      */
     @RequestMapping(path = "/voter", method = RequestMethod.POST)
-    public Voter voter(@RequestParam("email") String email, @RequestParam("password") String password) {
-        Voter voter = PersistenceServiceImpl.getInstance().getVoterDao().getByEmail(email);
+    public Voter voter(@RequestParam("login") String login, @RequestParam("password") String password) {
+        Voter voter = PersistenceServiceImpl.getInstance().getVoterDao().getByEmail(login);
 
         if (voter == null)
             return null; // TODO respond with 404
