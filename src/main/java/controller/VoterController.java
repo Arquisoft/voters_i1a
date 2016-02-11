367eae4d7915b11ccb7bb83724a61e485c493453
sergioifg94 <sergioifg94@gmail.com>
2016-02-11 13:11:02 +0100

    Controller accepts JSON requests. Created DTO for voters

--- src/main/java/controller/VoterController.java
+++ src/main/java/controller/VoterController.java
@@ -1,10 +1,8 @@
 package controller;
 
+import dto.VoterLogin;
 import model.Voter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.PersistenceServiceImpl;
 
 /**
@@ -16,17 +14,15 @@ public class VoterController {
     /**
      * Process a request to /voter. Checks that the given password matches a voter, and, if it does, respond with
      * data about the voter
-     * @param login Email of the voter
-     * @param password Password of the voter
      * @return Voter
      */
     @RequestMapping(path = "/voter", method = RequestMethod.POST)
-    public Voter voter(@RequestParam("login") String login, @RequestParam("password") String password) {
-        Voter voter = PersistenceServiceImpl.getInstance().getVoterDao().getByEmail(login);
+    public Voter voter(@RequestBody VoterLogin voterLogin) {
+        Voter voter = PersistenceServiceImpl.getInstance().getVoterDao().getByEmail(voterLogin.getLogin());
 
         if (voter == null)
             return null; // TODO respond with 404
-        if (!voter.getPassword().equals(password))
+        if (!voter.getPassword().equals(voterLogin.getPassword()))
             return null; // TODO respond with 404
 
         return voter;
